9ad3a3205604ad5950a60b23633fbde131747fd3
alfanaisy <alfanaisy7@gmail.com>
2024-05-24 16:05:39 +0700

    highligt selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -10,9 +10,10 @@ import useGenres, { Genre } from '../hooks/useGenres';
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -31,6 +32,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
             <Button
               variant={'link'}
               fontSize={'lg'}
+              fontWeight={selectedGenre?.id === genre.id ? 'bold' : 'normal'}
               onClick={() => onSelectGenre(genre)}
             >
               {genre.name}
