b61d59da408b69485b7a5353cf14a6f8f8dba612
alfanaisy <alfanaisy7@gmail.com>
2024-05-28 11:00:23 +0700

    build game attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,10 @@
 import { useParams } from 'react-router-dom';
 import useGetGame from '../hooks/useGetGame';
-import { Heading, Spinner, Text } from '@chakra-ui/react';
+import { Heading, SimpleGrid, Spinner, Text } from '@chakra-ui/react';
 import ExpandableText from '../components/ExpandableText';
+import DefinitionItem from '../components/DefinitionItem';
+import CriticScore from '../components/CriticScore';
+import GameAttributes from '../components/GameAttributes';
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -14,8 +17,8 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      {/* <Text>{game.description_raw}</Text> */}
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
